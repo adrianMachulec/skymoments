299ff43d75c4a4c878ced75f3382a5e0546f10bc
Adrian Machulec <adriandamianmachulec@gmail.com>
2023-07-08 13:27:53 +0200

    homepage content

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,5 +1,8 @@
 import styles from "./Homepage.module.scss";
 import homepageImg1 from "./../../assets/images/homepage/img1.jpg";
+import wikiImg1 from "./../../assets/images/wiki/wiki1.jpg";
+import portImg1 from "./../../assets/images/homepage/img2.jpg";
+import portImg2 from "./../../assets/images/homepage/img13.jpg";
 
 export default function Homepage(props) {
   return (
@@ -24,6 +27,51 @@ export default function Homepage(props) {
           swojego fotografa i mogę z Wami przeżywać najważniejsze oraz
           najpiekniejsze momenty życia.
         </p>
+      </div>
+
+      <div className={styles.homepageAboutMe}>
+        <img
+          src={wikiImg1}
+          alt="Zdjęcie Wiki"
+          className={styles.homepageAboutMeImg}
+        />
+        <div className={styles.homepageAboutMeLine}></div>
+        <p className={styles.homepageAboutMeText}>
+          Nazywam się Wiktoria i chciałabym Was zaprosić do mojego świata
+          fotografii, świata pięknych obrazów i magicznie niebiańskich momentów.
+          Fotografia to moja pasja ale oprócz niej lubię wiele innych rzeczy.
+          Jeżeli jesteście ciekawi jaką jestem osobą i chcecie dowiedzieć się
+          czegoś na mój temat to zapraszam Was do przeczytania paru zdań ,które
+          znajdują się w zakładce o mnie.
+        </p>
+      </div>
+
+      <div className={styles.homepagePortfolio}>
+        <h1>Moje portfolio</h1>
+        <div className={styles.homepagePortfolioContainer}>
+          <a
+            href="/portfolio"
+            className={styles.homepagePortfolioContainerElem}
+          >
+            <h2>Reportaże ślubne</h2>
+            <img
+              src={portImg1}
+              alt="Zdjęcie portfolio"
+              className={styles.homepagePortfolioContainerElemImg}
+            />
+          </a>
+          <a
+            href="/portfolio"
+            className={styles.homepagePortfolioContainerElem}
+          >
+            <h2>Sesje okolicznościowe</h2>
+            <img
+              src={portImg2}
+              alt="Zdjęcie portfolio"
+              className={styles.homepagePortfolioContainerElemImg}
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
